f7a99d71fb305825df96c6329a11a08c3cd64a07
Superomarking <71559522+Superomarking@users.noreply.github.com>
2020-12-08 22:52:17 +0300

    Add files via upload

---
+++ src/Superomarking/Report/Main.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Superomarking\Report;
+
+use pocketmine\event\Listener;
+
+use pocketmine\command\CommandSender;
+use pocketmine\command\Command;
+
+use pocketmine\plugin\PluginBase;
+
+use pocketmine\Player;
+
+use pocketmine\utils\TextFormat;
+
+class Main extends PluginBase implements Listener {
+
+    public function onEnable(){
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+		$this->getLogger()->info(TextFormat::GREEN . "Report by Superomarking Enabled");
+    }
+    public function onDisable(){
+        $this->getLogger()->info(TextFormat::RED . "Report by Superomarking Disabled");
+    }
+    public function onCommand(CommandSender $sender, Command $cmd, String $label, Array $args) : bool {
+        switch($cmd->getName()){
+            case "report":
+                if(!$sender instanceof Player){
+                    $sender->sendMessage(TextFormat::RED . "Please Use This Command In Game");
+                } else {
+                    if(empty($args[0])){
+                        $sender->sendMessage(TextFormat::RED . "Usage: /report <player> <reason>");
+                        return true;
+                    } else {
+                        if(empty($args[1])){
+                            $sender->sendMessage(TextFormat::RED . "Usage: /report <player> <reason>");
+                            return true;
+                        } else {
+                            if($this->getServer()->getPlayer($args[0]) instanceof Player){
+                                $sender->sendMessage(TextFormat::GREEN . "Thank You For Your Report");
+                                $this->report($sender, $args[0], $args[1]);
+                            } else {
+                            	$sender->sendMessage(TextFormat::RED . $args[0] . " Is Not Online Right Now"); 
+                            }
+                        }
+                    }
+                }
+        }
+        return true;
+    }
+    public function report(Player $player, $reason, $reported){
+        foreach($this->getServer()->getOnlinePlayers() as $players){
+            if($players->hasPermission("report.sender")){
+                $players->sendMessage(TextFormat::GREEN . $reported." was reported by ".$player->getName()." for ". $reason);
+            }
+        }
+    }
+}
